c9f2bb448af0bca90c11e68c1c6e8f066fac63a7
Buc.io <esteban.tese@gmail.com>
2023-02-16 11:37:16 -0600

    Update settings.lua

--- 2_config/nvim/lua/settings.lua
+++ 2_config/nvim/lua/settings.lua
@@ -1,17 +1,27 @@
-vim.opt.number = true
+-- setings
+vim.g.mapleader = ','
+
 vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.cursorline = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = false
+vim.opt.showmatch = true
+vim.opt.swapfile = false
+vim.opt.backup = false
 vim.opt.wrap = true
 vim.opt.breakindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = false
-vim.opt.showmode = false
+vim.opt.colorcolumn = "80"
+vim.opt.cursorcolumn = true
 vim.opt.termguicolors = true
+vim.opt.relativenumber = true
+vim.opt.autoindent = true
+vim.opt.signcolumn = "yes"
+vim.opt.completeopt = 'menuone,noselect'
+vim.opt.wildmenu = true
+vim.opt.wildoptions='pum'
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.cmd('colorscheme darcula')
